09fb42c6788cffe33883cf83b862bb649d44d465
Enderson Florian <eflorian@phoenixcalibrationdr.com>
2023-07-19 15:15:53 -0400

    Manejando Excepciones Via Pipeline

---
+++ Application/Exceptions/ValidationExceptions.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Exceptions
+{
+    public class ValidationExceptions : Exception
+    {
+        public List<string> Errors { get;}
+
+        public ValidationExceptions() :base("Se han producido uno o mas errores de validacion")
+        {
+            Errors = new List<string> ();
+        }
+        public ValidationExceptions(IEnumerable<ValidationFailure> failures) : this()
+        {
+            foreach (var failure in failures) 
+            {
+                Errors.Add(failure.ErrorMessage);
+            }
+        }
+    }
+}
